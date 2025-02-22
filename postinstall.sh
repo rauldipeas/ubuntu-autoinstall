f2ada03b93a73965ead0fe6958ae72cecb046129
Raul Dipeas <github.rauldipeas@lock.email>
2025-02-22 05:37:36 -0300

    ^autoinstall ^postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -5,7 +5,6 @@ sudo usermod -aG audio "$USER"
 sudo apt install python3-tk
 pipx install rtcqs
 
-flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
 flatpak install -y com.github.tchx84.Flatseal
 flatpak install -y fm.reaper.Reaper
 flatpak install -y org.gimp.GIMP
