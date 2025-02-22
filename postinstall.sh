c33bebd9516597bed6764bb9e4560c038e699e10
Raul Dipeas <github.rauldipeas@lock.email>
2025-02-22 04:15:35 -0300

    +postinst ^autoinstall

---
+++ postinstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+flatpak install -y com.github.tchx84.Flatseal
+flatpak install -y fm.reaper.Reaper
+flatpak install -y org.gimp.GIMP
+flatpak install -y org.kde.kdenlive
+flatpak install -y org.pipewire.Helvum
