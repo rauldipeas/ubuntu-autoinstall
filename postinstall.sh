202e0e1200cb2051b1d6c4bcb7608fea808bed60
Raul Dipeas <github.rauldipeas@lock.email>
2025-02-22 05:45:43 -0300

    ^postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -6,6 +6,7 @@ sudo apt install python3-tk
 pipx install rtcqs
 
 flatpak install -y com.github.tchx84.Flatseal
+flatpak install -y com.saivert.pwvucontrol
 flatpak install -y fm.reaper.Reaper
 flatpak install -y org.gimp.GIMP
 flatpak install -y org.kde.kdenlive
